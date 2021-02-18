7a7d1312ff0dbd26d76473b0176be89de7775b00
João <victorpow@live.com>
2021-02-18 20:01:23 -0300

    active storage configuration and some views for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,22 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
+  end
+
+  def create
+    @company = current_user.build_company(company_params)
+    redirect_to @company, notice: 'Empresa cadastrada com sucesso!' if @company.save
+    
+   
+    render :new, notice: 'oi'
   end
 
   def edit
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+  validates :name, :url, presence: true
+
+  has_one_attached :logo
 end
