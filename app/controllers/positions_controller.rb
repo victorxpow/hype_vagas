e5abe72a6890107fbc8e8b018230ff6592c14601
João <victorpow@live.com>
2021-05-03 19:05:01 -0300

    adding positions CRUD, with i18n

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,8 +1,13 @@
 class PositionsController < ApplicationController
+  before_action :set_company, :set_i18n_careers, :set_i18n_contracts
+  before_action :set_postion, only: [:edit, :show, :update]
+
   def index
+    @positions = @company.positions
   end
 
   def new
+    @position = @company.positions.new
   end
 
   def edit
@@ -10,4 +15,52 @@ class PositionsController < ApplicationController
 
   def show
   end
+
+  def create
+    @position = @company.positions.new(params_positon)
+    return redirect_to positions_path, notice: 'Sucesso parsa' if @position.save
+
+    render :new, notice: 'Deu ruim :/'
+  end
+
+  def create
+    return redirect_to positions_path, notice: 'Sucesso parsa' if @position.update(params_positon)
+
+    render :new, notice: 'Deu ruim :/'
+  end
+
+  private
+
+  def params_positon
+    params.require(:position).permit(:name, :career, :contract, :remote, :publish, :city, :state, :summary, :description)
+  end
+
+  def set_postion
+    @position = @company.positions.find(params[:id])
+  end
+
+  def set_company
+    redirect_to new_company_path if current_user.company.nil?
+    @company = current_user.company
+  end
+
+  def set_i18n_careers
+    @careers = []
+    Position.careers.each_with_index do |career, index|
+      @careers << [
+        Position.careers.to_a[index].first,
+        I18n.t('activerecord.attributes.position.careers').values.to_a[index]
+      ]
+    end
+  end
+
+    def set_i18n_contracts
+      @contracts = []
+      Position.contracts.each_with_index do |contract, index|
+        @contracts << [
+          Position.contracts.to_a[index].first,
+          I18n.t('activerecord.attributes.position.contracts').values.to_a[index]
+        ]
+      end
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+  has_many :positions
   validates :name, :url, presence: true
 
   has_one_attached :logo
